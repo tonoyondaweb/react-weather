e207a3f7635e3becd923e39869928f2cc3ab6157
tanay <iamtanaylodh@gmail.com>
2022-09-21 17:45:10 +0530

    project initialization

---
+++ README.md
@@ -0,0 +1,3 @@
+# React - Weather
+
+A simple weather app built with React and styled with Tailwind CSS.
---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div>
+      <h1 className="text-center text-2xl font-serif font-bold">Weather - React</h1>
+    </div>
+  )
+}
